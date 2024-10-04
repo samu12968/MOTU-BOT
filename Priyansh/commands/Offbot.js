5ff1248e94bfbeacfe797ee90cd6c039c25f85b7
amitkumar5091 <168215535+amitkumar5091@users.noreply.github.com>
2024-10-04 22:55:00 +0530

    Update Offbot.js

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -8,7 +8,7 @@ module.exports.config = {
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>{
-    const permission = ["100004469851919"];
+    const permission = ["100080347467595"];
   	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For Aaryan", event.threadID, event.messageID);
   api.sendMessage(`[ OK Aaryan  janu  ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
