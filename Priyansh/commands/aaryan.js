309680698d1fe7571d153690bb2456e63137ce6f
amitkumar5091 <168215535+amitkumar5091@users.noreply.github.com>
2024-10-05 09:07:10 +0530

    Update aaryan.js

--- Priyansh/commands/aaryan.js
+++ Priyansh/commands/aaryan.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("aaryan") || 
 react.includes("aryan")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗣𝗿𝗼𝗳𝗶𝗹𝗲 𝗛𝗲𝗿𝗲★\n\n✦𝗔𝗮𝗿𝘆𝗮𝗻 𝗦𝗵𝗮𝗿𝗮𝗯𝗶 ✦\n\n https://www.facebook.com/ARYAN.SHARABI.HERE\n\n☞★★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 :  𝐌𝐫..𝐀𝐚𝐫𝐲𝐚𝐧🩷🪽✦`🥀🦋",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗣𝗿𝗼𝗳𝗶𝗹𝗲 𝗛𝗲𝗿𝗲★\n\n✦𝗔𝗮𝗿𝘆𝗮𝗻 𝗦𝗵𝗮𝗿𝗮𝗯𝗶 ✦\n\n https://www.facebook.com/ARYAN.SHARABI.HERE \n\n☞★★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 :  𝐌𝐫..𝐀𝐚𝐫𝐲𝐚𝐧🩷🪽✦`🥀🦋",
 				attachment: fs.createReadStream(__dirname + `/noprefix/admin.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
